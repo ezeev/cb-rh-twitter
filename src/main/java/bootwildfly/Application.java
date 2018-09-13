c8bb9534652305344a5b749898fc7f1905655b96
Evan Pease <evan.pease@gmail.com>
2018-09-13 15:19:04 -0700

    test

--- src/main/java/bootwildfly/Application.java
+++ src/main/java/bootwildfly/Application.java
@@ -3,11 +3,19 @@ package bootwildfly;
 import org.springframework.boot.SpringApplication;
 import java.util.Arrays;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
