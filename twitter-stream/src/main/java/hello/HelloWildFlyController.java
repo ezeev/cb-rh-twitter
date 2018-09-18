bf8b726e2ff8f999977738b901d0182188536ec7
Evan Pease <evan.pease@gmail.com>
2018-09-18 15:11:39 -0700

    first commit

--- twitter-stream/src/main/java/hello/HelloWildFlyController.java
+++ twitter-stream/src/main/java/hello/HelloWildFlyController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWildFlyController {
 
-
     @RequestMapping("/hello")
     public String sayHello(){
         return ("Greetings from Spring Boot!");
