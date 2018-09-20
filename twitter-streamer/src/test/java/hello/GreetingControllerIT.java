85d7ea8aba27a05e1cf4a5e709e67294aa7d131d
Evan Pease <evan.pease@gmail.com>
2018-09-20 12:45:57 -0700

    test

--- twitter-streamer/src/test/java/hello/GreetingControllerIT.java
+++ twitter-streamer/src/test/java/hello/GreetingControllerIT.java
@@ -4,18 +4,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.URL;
 
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matcher.*;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
