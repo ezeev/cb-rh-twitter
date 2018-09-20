a8f81758e68138224f444235b1a570591c1ffa51
Evan Pease <evan.pease@gmail.com>
2018-09-20 06:50:14 -0700

    test

--- twitter-streamer/src/main/java/com/couchbase/CouchbaseSink.java
+++ twitter-streamer/src/main/java/com/couchbase/CouchbaseSink.java
@@ -4,6 +4,8 @@ import com.couchbase.client.java.*;
 import com.couchbase.client.java.document.*;
 import com.couchbase.client.java.document.json.*;
 import com.couchbase.client.java.query.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,8 @@ import javax.annotation.PostConstruct;
 @Service
 public class CouchbaseSink implements TweetSink {
 
+
+    private static final Logger log = LoggerFactory.getLogger(CouchbaseSink.class);
 
     private Cluster cluster;
     private Bucket tweetBucket;
@@ -39,6 +43,12 @@ public class CouchbaseSink implements TweetSink {
     @Override
     @PostConstruct
     public void connect() {
+
+        if (couchbaseAddress == null) {
+            log.warn("Missing COUCHBASE_CLUSTER environment variable. Tweets will not be saved!");
+            return;
+        }
+
         cluster = CouchbaseCluster.create(couchbaseAddress);
         cluster.authenticate(couchbaseUserName, couchbasePassword);
         tweetBucket = cluster.openBucket(bucketName);
@@ -53,6 +63,11 @@ public class CouchbaseSink implements TweetSink {
     }
 
     public void addTweet(String msg) {
+
+        if (couchbaseAddress == null) {
+            return;
+        }
+
         JsonObject tweet = JsonObject.fromJson(msg);
         if (tweet.containsKey("id")) {
             tweetBucket.upsert(JsonDocument.create(tweet.get("id").toString(), tweet));
